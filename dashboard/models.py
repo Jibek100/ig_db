fb9549e77bed0a4d226b652daf7df70f41d270c2
Nuradil <mr.nuradil@gmail.com>
2021-09-16 19:27:22 +0600

    add links to entities

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,17 +2,24 @@ from django.db import models
 from django.utils import timezone
 
 class Profile(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, primary_key=True)
     bio = models.TextField()
     type = models.TextField()
 
 class Post(models.Model):
+    post_id = models.IntegerField(primary_key=True)
     text = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     like_count = models.IntegerField()
+    comment_count = models.IntegerField()
+    engament = models.IntegerField()
+    impression = models.IntegerField()
+    reach = models.IntegerField()
+    saved = models.IntegerField()
 
 class Comment(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -20,6 +27,7 @@ class Comment(models.Model):
     like_count = models.IntegerField()
 
 class Reply(models.Model):
+    reply_id = models.IntegerField(primary_key=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
