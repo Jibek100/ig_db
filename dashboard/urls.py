d90ed8949c9ab909a9b47c027eb5d2f74525a1d8
Zhibek <zhibek.tazhdavletova@nu.edu.kz>
2021-11-19 01:12:18 +0600

    merged search engine

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='dashboard-home'),
+    path('searchcomment/', views.searchComment, name="dashboard-search-comment"),
     path('classifycomment/<int:pk>', views.classifyComment, name="dashboard-classify-comment"),
     path('classifycomments/', views.classifyComments, name="dashboard-classify-comments"),
     path('profiles/', views.profile, name="dashboard-profiles"),
